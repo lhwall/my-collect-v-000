b54487ba475a1099b10bd6dc2b777ec6da4d3a36
Lucy Wall <lhwall@gmail.com>
2018-03-23 14:07:57 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,10 +2,8 @@ begin def my_collect(array)
   updated_array = []
   i = 0 
   while array.length > i
-  
-  yield(array[i])
-  updated_array << yield
-  i += 1
+    updated_array << yield(array[i])
+ i += 1
 end
 updated_array
 end
